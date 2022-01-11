0d083b5a5346bdb76e7159a237bf5b0ebb48fff4
Harris Kim <seura710@gmail.com>
2022-01-11 11:45:11 -0500

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     user = User.find(session[:user_id])
     render json: user
   end
